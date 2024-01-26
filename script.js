3f8d31f32a709a237f715e64173f831421b20d05
Kylian B <kylian.belnou@orange.fr>
2024-01-26 14:23:37 +0100

    Suppression des caractères spéciaux et lettre dans les inputs de chiffres

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ import readlineSync from 'readline-sync';
 
 console.log("------- Meilleure calculatrice du monde -------");
 
+function isNumeric(value) {
+    return /^[0-9]+$/.test(value);
+}
+
 function calcCmd() {
     var cmd = readlineSync.question("Quelle opération voulez-vous faire ? (add, sub, mul, div, mod) ");
 
-    var nb1 = parseInt(readlineSync.question("Premier nombre "));
-    var nb2 = parseInt(readlineSync.question("Deuxième nombre "));
+    var nb1 = readlineSync.question("Premier nombre ");
+    var nb2 = readlineSync.question("Deuxième nombre ");
 
-    if (isNaN(nb1) || isNaN(nb2)) {
+    // Vérifie si nb1 et nb2 contiennent uniquement des chiffres
+    if (!isNumeric(nb1) || !isNumeric(nb2)) {
         console.log("Veuillez entrer des nombres valides.");
         return;
     }
+
+    nb1 = parseInt(nb1);
+    nb2 = parseInt(nb2);
 
     var result = 0;
 
@@ -22,9 +30,8 @@ function calcCmd() {
     } else if (cmd == "mul") {
         result = nb1 * nb2;
     } else if (cmd == "mod") {
-        result = nb1 % nb2;
         if (nb2 !== 0) {
-            result = nb1 / nb2;
+            result = nb1 % nb2;
         } else {
             console.log("Modulo par zéro impossible.");
             return;
